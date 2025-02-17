93dfac93a378d5f8aa487d78525d0a0773ec5398
Julian Labbe <jlabbe@calpoly.edu>
2025-02-17 14:30:48 -0800

    checking cors issue with no backend connected

--- packages/express-backend/api/backend.js
+++ packages/express-backend/api/backend.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import services from "../data-services.js";
+//import services from "../data-services.js";
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,11 +13,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/form", (req, res) => {
-  const name = req.query.name;
+  res.send().status(201);
+  /*  const name = req.query.name;
   const email = req.query.email;
   const message = req.query.message;
   if (name != undefined && email != undefined && message != undefined) {
-    console.log("correct data")
+    console.log("correct data");
     let dataToAdd = req.body;
     services
       .addData(dataToAdd)
@@ -28,9 +30,9 @@ app.post("/form", (req, res) => {
         res.status(404);
       });
   } else {
-    console.log("incorrect data")
+    console.log("incorrect data");
     res.status(404);
-  }
+  }*/
 });
 
 export default app;
